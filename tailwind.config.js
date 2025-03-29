06abc18c469f6b4f7877c3778d9f0d3c945432b1
imransyed011220 <imransd082@gmail.com>
2025-03-29 15:47:29 +0530

    Add tailwind.config.js 1743243449801

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./timetable.js",
+    "./script.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+      }
+    },
+  },
+  plugins: [],
+}
